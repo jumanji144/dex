a91a0c81f35e412486e7416a8f3593cbc4231e65
Justus Garbe <gihihoh@gmail.com>
2024-02-08 21:43:29 +0100

    impr: add method handle value

--- dex-core/src/main/java/me/darknet/dex/file/value/MethodHandleValue.java
+++ dex-core/src/main/java/me/darknet/dex/file/value/MethodHandleValue.java
@@ -1,6 +1,31 @@
 package me.darknet.dex.file.value;
 
-public record MethodHandleValue() implements Value {
+import me.darknet.dex.file.DexMapAccess;
+import me.darknet.dex.file.items.MethodHandleItem;
+import me.darknet.dex.io.Input;
+import me.darknet.dex.io.Output;
+
+import java.io.IOException;
+
+public record MethodHandleValue(MethodHandleItem item) implements Value {
+
+    public static final ValueCodec<MethodHandleValue> CODEC = new ValueCodec<>() {
+        @Override
+        public MethodHandleValue read(Input input, DexMapAccess context) throws IOException {
+            return new MethodHandleValue(context.methodHandles().get((int) input.readUnsignedInt()));
+        }
+
+        @Override
+        public void write(MethodHandleValue value, Output output, DexMapAccess context) throws IOException {
+            output.writeByte(((size() - 1) << 5) | value.type());
+            output.writeInt(context.methodHandles().indexOf(value.item));
+        }
+
+        @Override
+        public int size() {
+            return 4;
+        }
+    };
 
     @Override
     public int type() {
--- dex-core/src/main/java/me/darknet/dex/file/value/Value.java
+++ dex-core/src/main/java/me/darknet/dex/file/value/Value.java
@@ -18,7 +18,15 @@ public interface Value {
     }
 
     Map<Integer, ValueCodec<?>> CODECS = Map.ofEntries(
-      Map.entry(0x00, ByteValue.CODEC) // ByteValue
+      Map.entry(0x00, ByteValue.CODEC), Map.entry(0x02, ShortValue.CODEC),
+        Map.entry(0x03, CharValue.CODEC), Map.entry(0x04, IntValue.CODEC),
+        Map.entry(0x06, LongValue.CODEC), Map.entry(0x10, FloatValue.CODEC),
+        Map.entry(0x11, DoubleValue.CODEC), Map.entry(0x15, MethodTypeValue.CODEC),
+        Map.entry(0x16, MethodHandleValue.CODEC), Map.entry(0x17, StringValue.CODEC),
+        Map.entry(0x18, TypeValue.CODEC), Map.entry(0x19, FieldValue.CODEC),
+        Map.entry(0x1a, MethodValue.CODEC), Map.entry(0x1b, EnumValue.CODEC),
+        Map.entry(0x1c, ArrayValue.CODEC), Map.entry(0x1d, AnnotationValue.CODEC),
+        Map.entry(0x1e, NullValue.CODEC), Map.entry(0x1f, BoolValue.CODEC)
     );
 
     int type();
@@ -26,19 +34,20 @@ public interface Value {
     ContextCodec<Value, DexMapAccess> CODEC = new ContextCodec<>() {
 
         private Input zeroExtend(Input input, int size, int targetSize) throws IOException {
-            ByteBuffer buffer = ByteBuffer.allocate(targetSize);
+            ByteBuffer buffer = ByteBuffer.allocate(targetSize).order(input.order());
             buffer.put(input.readBytes(size));
+            buffer.position(0);
             return Input.wrap(buffer);
         }
 
         @Override
         public Value read(Input input, DexMapAccess context) throws IOException {
             int value = input.readUnsignedByte();
-            // encoding, value = (byte) (size << 5 | type)
-            int type = value & 0x1f;
-            int size = value >> 5;
-            ValueCodec<?> codec = CODECS.get(type);
-            Input passed = codec.size() == 0 ? input : zeroExtend(input, size, codec.size());
+            // (value_arg << 5) | value_type
+            int value_type = value & 0x1f;
+            int value_arg = value >> 5;
+            ValueCodec<?> codec = CODECS.get(value_type);
+            Input passed = codec.size() == 0 ? input : zeroExtend(input, value_arg+1, codec.size());
             return codec.read(passed, context);
         }
 
