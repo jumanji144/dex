d4aa5f000719ff54675740fd39474af317772efa
Justus Garbe <gihihoh@gmail.com>
2024-02-08 21:45:02 +0100

    misc: update missing formats

--- dex-core/src/main/java/me/darknet/dex/file/items/ClassDataItem.java
+++ dex-core/src/main/java/me/darknet/dex/file/items/ClassDataItem.java
@@ -17,10 +17,10 @@ public record ClassDataItem(List<EncodedField> staticFields, List<EncodedField> 
     public static final ItemCodec<ClassDataItem> CODEC = new ItemCodec<>() {
         @Override
         public ClassDataItem read0(Input input, DexMapAccess context) throws IOException {
-            int staticFieldsSize = (int) input.readULeb128();
-            int instanceFieldsSize = (int) input.readULeb128();
-            int directMethodsSize = (int) input.readULeb128();
-            int virtualMethodsSize = (int) input.readULeb128();
+            int staticFieldsSize = input.readULeb128();
+            int instanceFieldsSize = input.readULeb128();
+            int directMethodsSize = input.readULeb128();
+            int virtualMethodsSize = input.readULeb128();
 
             List<EncodedField> staticFields = new ArrayList<>(staticFieldsSize);
             List<EncodedField> instanceFields = new ArrayList<>(instanceFieldsSize);
@@ -29,25 +29,25 @@ public record ClassDataItem(List<EncodedField> staticFields, List<EncodedField> 
 
             int lastFieldIndex = 0;
             for (int i = 0; i < staticFieldsSize; i++) {
-                int fieldIndexDiff = (int) input.readULeb128();
-                int accessFlags = (int) input.readULeb128();
+                int fieldIndexDiff = input.readULeb128();
+                int accessFlags = input.readULeb128();
                 lastFieldIndex += fieldIndexDiff;
                 staticFields.add(new EncodedField(context.fields().get(lastFieldIndex), accessFlags));
             }
 
             lastFieldIndex = 0;
             for (int i = 0; i < instanceFieldsSize; i++) {
-                int fieldIndexDiff = (int) input.readULeb128();
-                int accessFlags = (int) input.readULeb128();
+                int fieldIndexDiff = input.readULeb128();
+                int accessFlags = input.readULeb128();
                 lastFieldIndex += fieldIndexDiff;
                 instanceFields.add(new EncodedField(context.fields().get(lastFieldIndex), accessFlags));
             }
 
             int lastMethodIndex = 0;
             for (int i = 0; i < directMethodsSize; i++) {
-                int methodIndexDiff = (int) input.readULeb128();
-                int accessFlags = (int) input.readULeb128();
-                int codeOffset = (int) input.readULeb128();
+                int methodIndexDiff = input.readULeb128();
+                int accessFlags = input.readULeb128();
+                int codeOffset = input.readULeb128();
                 lastMethodIndex += methodIndexDiff;
                 CodeItem code = codeOffset == 0 ? null : CodeItem.CODEC.read(input.slice(codeOffset), context);
                 directMethods.add(new EncodedMethod(context.methods().get(lastMethodIndex), accessFlags, code));
@@ -55,9 +55,9 @@ public record ClassDataItem(List<EncodedField> staticFields, List<EncodedField> 
 
             lastMethodIndex = 0;
             for (int i = 0; i < virtualMethodsSize; i++) {
-                int methodIndexDiff = (int) input.readULeb128();
-                int accessFlags = (int) input.readULeb128();
-                int codeOffset = (int) input.readULeb128();
+                int methodIndexDiff = input.readULeb128();
+                int accessFlags = input.readULeb128();
+                int codeOffset = input.readULeb128();
                 lastMethodIndex += methodIndexDiff;
                 CodeItem code = codeOffset == 0 ? null : CodeItem.CODEC.read(input.slice(codeOffset), context);
                 virtualMethods.add(new EncodedMethod(context.methods().get(lastMethodIndex), accessFlags, code));
--- dex-core/src/main/java/me/darknet/dex/file/items/ClassDefItem.java
+++ dex-core/src/main/java/me/darknet/dex/file/items/ClassDefItem.java
@@ -10,7 +10,8 @@ import java.util.Collections;
 import java.util.List;
 
 public record ClassDefItem(TypeItem type, int access, TypeItem superType, List<TypeItem> interfaces,
-                           StringItem sourceFile, ClassDataItem classData)
+                           StringItem sourceFile, AnnotationsDirectoryItem directory, ClassDataItem classData,
+                           EncodedArrayItem staticValues)
         implements Item {
 
     public static ItemCodec<ClassDefItem> CODEC = new ItemCodec<ClassDefItem>() {
@@ -35,13 +36,20 @@ public record ClassDefItem(TypeItem type, int access, TypeItem superType, List<T
             int sourceFileIndex = input.readInt();
             StringItem sourceFile = sourceFileIndex == -1 ? null : context.strings().get(sourceFileIndex);
 
-            int annotationsOffset = input.readInt(); // TODO: annotations_directory_item
+            int annotationsOffset = input.readInt();
+            AnnotationsDirectoryItem directory = annotationsOffset == 0
+                    ? null
+                    : AnnotationsDirectoryItem.CODEC.read(input.slice(annotationsOffset), context);
             int classDataOffset = input.readInt();
             ClassDataItem classData = ClassDataItem.CODEC.read(input.slice(classDataOffset), context);
 
-            int staticValuesOffset = input.readInt(); // TODO: encoded_array_item
+            int staticValuesOffset = input.readInt();
+            EncodedArrayItem staticValues = staticValuesOffset == 0
+                    ? null
+                    : EncodedArrayItem.CODEC.read(input.slice(staticValuesOffset), context);
 
-            return new ClassDefItem(type, accessFlags, superType, interfaces, sourceFile, classData);
+            return new ClassDefItem(type, accessFlags, superType, interfaces, sourceFile, directory, classData,
+                    staticValues);
         }
 
         @Override
