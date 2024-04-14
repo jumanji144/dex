a4169d062dc66301003314001aee03c55e2f5eee
Justus Garbe <gihihoh@gmail.com>
2024-04-14 23:49:28 +0200

    fix: removing old debugging code

--- dex-core/src/main/java/me/darknet/dex/Dex.java
+++ dex-core/src/main/java/me/darknet/dex/Dex.java
@@ -1,19 +1,4 @@
 package me.darknet.dex;
 
-import me.darknet.dex.codecs.DexHeaderCodec;
-import me.darknet.dex.file.DexHeader;
-import me.darknet.dex.io.Input;
-
-import java.io.IOException;
-import java.io.InputStream;
-
 public class Dex {
-
-    public static void main(String[] args) throws IOException {
-        InputStream stream = Dex.class.getClassLoader().getResourceAsStream("classes.dex");
-        DexHeaderCodec codec = new DexHeaderCodec();
-        DexHeader builder = codec.read(Input.wrap(stream.readAllBytes()));
-        return;
-    }
-
 }
